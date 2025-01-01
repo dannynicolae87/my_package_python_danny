966e6b79ade655d9f801945f73658a116c689f18
Tarta Daniel Nicolae <dannynicolaeboy87@gmail.com>
2025-01-02 00:19:10 +0200

    modified requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="salutare",
+    name="unique_code_generator",
     version="0.1.0",
     author="Tarta Daniel Nicolae",
     author_email="dannynicolaeboy87@gmail.com",
